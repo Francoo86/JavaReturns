397dd2e7d504af7ed7e987f99c39e4a5a5070bbe
José Sandoval <josesand99v@gmail.com>
2024-04-10 16:00:39 +0000

    JAVA OVERDOSE

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # JavaReturns
 Sockets and UDP stuff for DB.
+
+
+pa compilar ^C@Krosvick ➜ /workspaces/JavaReturns (main) $ jacva -classpath ".:sqlite-jdbc-3.45.2.0.jar:slf4j-api-1.7.36.jar" src/Servidor/ServidorUDP.java
+@Krosvick ➜ /workspaces/JavaReturns (main) $ java -classpath ".:sqlite-jdbc-3.45.2.0.jar:slf4j-api-1.7.36.jar" src/Servidor/ServidorUDP.java
---
+++ src/Servidor/prueba.java
@@ -0,0 +1,26 @@
+package Servidor;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+ 
+public class prueba {
+ 
+   public static void main ( String args[] ){
+ 
+      Connection connection = null;
+ 
+      try{
+         connection = DriverManager.getConnection( "jdbc:sqlite:sample.db" );
+         Statement statement = connection.createStatement();
+         if ( connection != null ){
+            System.out.println("Conexión exitosa!");
+         }
+      }
+      catch ( Exception ex ) {
+         System.err.println( ex.getClass().getName() + ": " + ex.getMessage() );
+         System.out.println("Error en la conexión");
+      }
+   }
+ 
+}
