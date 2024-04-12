b35436773c8334f0a9b7c109a79cc495a8ba8000
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-04-11 21:54:32 -0400

    fix client udp by hardcoding stuff aahhh

--- src/cliente/ClienteUDP.java
+++ src/cliente/ClienteUDP.java
@@ -2,14 +2,17 @@ import java.net.*;
 import java.io.*;
 
 public class ClienteUDP {
+    static final String HOSTNAME = "localhost";
+    static final String TEST_MESSAGE = "hola a todos";
     public static void main(String args[]) {
         try {
             DatagramSocket unSocket = new DatagramSocket();
             unSocket.setSoTimeout(5000); // Establecer tiempo de espera de 5 segundos
-            byte [] m = args[0].getBytes();
-            InetAddress unHost = InetAddress.getByName(args[1]);
+            byte [] m = TEST_MESSAGE.getBytes();
+            System.out.println(m);
+            InetAddress unHost = InetAddress.getByName(HOSTNAME);
             int puertoServidor = 6789;
-            DatagramPacket peticion = new DatagramPacket(m, args[0].length(), unHost, puertoServidor); 
+            DatagramPacket peticion = new DatagramPacket(m, TEST_MESSAGE.length(), unHost, puertoServidor);
             unSocket.send(peticion);
             byte[] buffer = new byte[1000];
             DatagramPacket respuesta = new DatagramPacket(buffer, buffer.length);
