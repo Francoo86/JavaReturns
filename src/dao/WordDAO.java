9ab61b05daccb162012330dd00c0dba3bf148764
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-04-13 02:25:39 -0400

    add worddao
    tomorrow i will rush this

---
+++ src/dao/WordDAO.java
@@ -0,0 +1,74 @@
+package dao;
+
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.stmt.QueryBuilder;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.List;
+
+public class WordDAO implements IEntityDAO<Word, String> {
+    private Dao<Word, String> wordManager;
+    private QueryBuilder<Word, String> queryBuilder;
+
+    public WordDAO(ConnectionSource connSource) {
+        try {
+            wordManager = DaoManager.createDao(connSource, Word.class);
+            queryBuilder = wordManager.queryBuilder();
+
+            TableUtils.createTableIfNotExists(connSource, Word.class);
+        }
+        catch(SQLException e) {
+            System.out.printf("WordDAO: Can't initialize DAO because: %s", e);
+        }
+    }
+
+    @Override
+    public void add(Word entity) {
+
+    }
+
+    @Override
+    public void update(Word entity) {
+
+    }
+
+    @Override
+    public Word findByWordName(String wordName) {
+        return null;
+    }
+
+    @Override
+    public void delete(Word entityId) {
+
+    }
+
+    @Override
+    public List findAll() {
+        try {
+            List<Word> allWords = wordManager.queryForAll();
+            return allWords;
+        }
+        catch (SQLException e) {
+            System.out.printf("Can't get all elements of WordDAO because: %s", e);
+        }
+
+        return null;
+    }
+
+    @Override
+    public Word findUnique(String wordName) {
+        try {
+            Word tempWord = queryBuilder.where().eq("wordName", wordName).queryForFirst();
+            return tempWord;
+        }
+        catch(SQLException e) {
+            System.out.printf("WordDAO: Can't find element %s because %s", wordName, e);
+        }
+
+        return null;
+    };
+}
