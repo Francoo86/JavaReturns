d4bc722098fe58ef2fdf5d4b1e138a180525e86c
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-04-13 02:25:15 -0400

    add criteria method

--- src/dao/IEntityDAO.java
+++ src/dao/IEntityDAO.java
@@ -2,10 +2,11 @@ package dao;
 
 import java.util.Collection;
 
-public interface IEntityDAO <T> {
+public interface IEntityDAO <T, U> {
     void add(T entity);
     void update(T entity);
     T findByWordName(String wordName);
     void delete(T entityId);
     Collection<T> findAll();
+    T findUnique(U criteria);
 }
