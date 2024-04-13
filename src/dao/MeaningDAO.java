1a6c702c72036765fc0b9b5eb0db9eaaf534a76a
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-04-13 14:12:43 -0400

    complete MeaningDAO

--- src/dao/MeaningDAO.java
+++ src/dao/MeaningDAO.java
@@ -2,6 +2,7 @@ package dao;
 
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.stmt.PreparedQuery;
 import com.j256.ormlite.stmt.QueryBuilder;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
@@ -12,7 +13,7 @@ import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
 
-public class MeaningDAO implements IEntityDAO<WordDefinition, String> {
+public class MeaningDAO implements IEntityDAO<WordDefinition, Integer> {
     private Dao<WordDefinition, String> meaningManager;
     private QueryBuilder<WordDefinition, String> queryBuilder;
 
@@ -39,7 +40,7 @@ public class MeaningDAO implements IEntityDAO<WordDefinition, String> {
             meaningManager.create(entity);
         }
         catch (SQLException e) {
-            System.out.printf("MeaningDAO: Can't add definition for %s because %s", entity.getWord().getWordName(), entity)
+            System.out.printf("MeaningDAO: Can't add definition for %s because %s", entity.getWord().getWordName(), entity);
         }
         catch (IOException e) {
             System.out.printf("MeaningDAO: Needs to have a word to be added.");
@@ -47,22 +48,40 @@ public class MeaningDAO implements IEntityDAO<WordDefinition, String> {
     }
 
     @Override
-    public WordDefinition findByWordName(String wordName) {
+    public List findAll() {
+        try {
+            List<WordDefinition> allDefs = meaningManager.queryForAll();
+            return allDefs;
+        }
+        catch (SQLException e) {
+            System.out.printf("Can't get all elements of MeaningDAO because: %s", e);
+        }
+
         return null;
     }
 
     @Override
-    public Collection<WordDefinition> findAll() {
-        return List.of();
+    public WordDefinition findUnique(Integer criteria) {
+        //return null;
+        try {
+            WordDefinition wordDef = meaningManager.queryForId(Integer.toString(criteria));
+            return wordDef;
+        }
+        catch(SQLException e) {
+            System.out.printf("MeaningDAO: Can't find word by ID (%s) because: ", criteria, e);
+        }
+        return null;
     }
 
-    @Override
-    public WordDefinition findUnique(String criteria) {
+    public Collection<WordDefinition> findSimilar(String criteria) {
+        //return null;
         try {
+            Collection<WordDefinition> currDef = queryBuilder.where().like("def", "'%"+criteria+"%'").query();
+            return currDef;
+        }catch (SQLException e) {
+            System.out.printf("MeaningDAO: We can't search similar definitions holding %s because: %e", criteria, e);
+        }
 
-        }
-        catch (SQLException e) {
-            System.out.printf("xdddd");
-        }
+        return null;
     }
 }
