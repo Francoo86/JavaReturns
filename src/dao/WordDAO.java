178dd29512a42801c9ce1e366f3384f1e11f08a7
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-04-13 14:12:56 -0400

    complete WordDAO

--- src/dao/WordDAO.java
+++ src/dao/WordDAO.java
@@ -6,16 +6,23 @@ import com.j256.ormlite.stmt.QueryBuilder;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
 
+import java.io.IOError;
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
 
 public class WordDAO implements IEntityDAO<Word, String> {
+    public static final int MAX_DEFINITIONS = 5;
     private Dao<Word, String> wordManager;
     private QueryBuilder<Word, String> queryBuilder;
+    private MeaningDAO defManager;
 
     public WordDAO(ConnectionSource connSource) {
         try {
+            defManager = new MeaningDAO(connSource);
+
+            //This better.
             wordManager = DaoManager.createDao(connSource, Word.class);
             queryBuilder = wordManager.queryBuilder();
 
@@ -35,11 +42,6 @@ public class WordDAO implements IEntityDAO<Word, String> {
             System.out.printf("WordDAO: Can't add word because: %s", e);
         }
     };
-
-    @Override
-    public Word findByWordName(String wordName) {
-        return null;
-    }
 
     @Override
     public List findAll() {
@@ -66,4 +68,33 @@ public class WordDAO implements IEntityDAO<Word, String> {
 
         return null;
     };
+
+    public void addMeaning(String currentWord, String meaning) {
+        try {
+            Word wordObj = findUnique(currentWord);
+            if(wordObj == null) {
+                return;
+            }
+
+            Collection<WordDefinition> actualDefs = wordObj.getDefinitions();
+
+            if(actualDefs.size() >= MAX_DEFINITIONS) {
+                throw new IOException(String.format("WordDAO: A word can't have more than %s definitions.", MAX_DEFINITIONS));
+            }
+
+            WordDefinition def = new WordDefinition();
+            def.setWord(wordObj);
+            def.setDef(meaning);
+            defManager.add(def);
+        }catch (IOException e) {
+            System.out.println(e);
+        }
+    }
+
+    public Collection<WordDefinition> getMeanings(String wordName) {
+        Word word = findUnique(wordName);
+        if(word == null) return null;
+
+        return word.getDefinitions();
+    }
 }
