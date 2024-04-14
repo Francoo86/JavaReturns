8daf57ae5385131d916386aa4ab4530b8be7ac2f
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-04-14 02:10:29 -0400

    cleanup main files

--- src/MainClient.java
+++ src/MainClient.java
@@ -1,27 +1,8 @@
-import Servidor.services.DictionaryService;
-import com.j256.ormlite.jdbc.JdbcConnectionSource;
-import com.j256.ormlite.support.ConnectionSource;
 import genericmenu.MenuApp;
-
-import java.sql.SQLException;
 
 public class MainClient {
     public static void main(String[] args) {
         MenuApp menu = new MenuApp();
         menu.runMenu();
     }
-
-    /*
-    private static void testPersonDao() {
-        String dbUrl = "jdbc:sqlite:sample.db";
-        try {
-            ConnectionSource connSource = new JdbcConnectionSource(dbUrl);
-            DictionaryService dictService = new DictionaryService(connSource);
-            //dictService.addWord("porotos");
-            dictService.addDefinition("porotos", "prueba 5to significado");
-            dictService.addDefinition("porotos", "hola si");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }*/
 }
--- src/MainServer.java
+++ src/MainServer.java
@@ -1,8 +1,7 @@
 import Servidor.UDPServer;
-import genericmenu.MenuApp;
 
 public class MainServer {
-    private static int PORT = 6789;
+    private static final int PORT = 6789;
     public static void main(String[] args) {
         UDPServer server = new UDPServer("jdbc:sqlite:sample.db", PORT);
         server.listenClients();
