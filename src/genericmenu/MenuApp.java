83749942a30aafe4b5db60f373704a6de74a4f1f
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-04-13 21:05:45 -0400

    ditto

--- src/genericmenu/MenuApp.java
+++ src/genericmenu/MenuApp.java
@@ -2,6 +2,7 @@ package genericmenu;
 
 import Servidor.UDPServer.Services;
 import cliente.UDPClient;
+import shd_utils.ParseHelpers;
 
 import java.util.Scanner;
 
@@ -26,8 +27,31 @@ public class MenuApp {
         System.out.println("4. Cerrar programa.");
     }
 
-    private void searchWord(String word) {
+    private void prepareWordSearch() {
+        String word = sc.next().trim();
 
+        if(!ParseHelpers.isValidWord(word)) {
+            System.out.println("La palabra tiene que ser mayor a 3 caracteres.");
+            return;
+        }
+
+        //String content = ParseHelpers.createContents(Services.SEARCH_WORD, word);
+        //client.sendMessage(content);
+    }
+
+    private void prepareWordAdding() {
+        System.out.println("Introduzca la palabra a colocar significado.");
+        String word = sc.nextLine().trim();
+        System.out.println("Introduzca el signficado correspondiente.");
+        String meaning = sc.nextLine().trim();
+
+        if(!ParseHelpers.isValidWord(word) || !ParseHelpers.isValidWord(meaning)) {
+            System.out.println("No se permiten definiciones vacias o palabras muy pequeñas (menores a 3 caracteres).");
+            return;
+        }
+
+        //String content = ParseHelpers.createContents(Services.ADD_MEANING, word, meaning);
+        //client.sendMessage(content);
     }
 
     //you can't return onto while true without breaking it.
@@ -39,12 +63,24 @@ public class MenuApp {
             return false;
         }
 
+        //goofy ahh reset nextline.
+        sc.nextLine();
+
         Services service = SERVICES[input - 1];
+
+        System.out.println("Servicio actual: " + service.name());
 
         switch (service){
             case SEARCH_WORD:
+                prepareWordSearch();
+                break;
             case ADD_MEANING:
+                prepareWordAdding();
+                break;
             case CHANGE_CURRENCY:
+                break;
+                //TODO: Implement.
+            //HACK: Add this for avoiding thinking too much.
             case NULL_SERVICE:
                 System.out.println("Saliendo del menu...");
                 return true;
