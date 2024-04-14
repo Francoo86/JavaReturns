7240886e9b6b5ec0e02345034d3b278d4b25ba03
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-04-14 14:33:17 -0400

    needs to be like that!!!!!

--- src/Servidor/serializables/CurrencyResponse.java
+++ src/Servidor/serializables/CurrencyResponse.java
@@ -5,12 +5,12 @@ import java.util.HashMap;
 //vietnam flashbacks
 public class CurrencyResponse {
     private String provider;
-    private String warningUpgrade;
+    private String WARNING_UPGRADE_TO_V6;
     private String terms;
     //This is only the one relevant for the project.
-    private String currencyBase;
-    private String requestedDate;
-    private int lastUpdatedTime;
+    private String base;
+    private String date;
+    private int time_last_updated;
     //Also this one.
     private HashMap<String, Double> rates;
 
@@ -19,7 +19,6 @@ public class CurrencyResponse {
     }
 
     public String getCurrencyBase() {
-        return currencyBase;
+        return base;
     }
-
 }
