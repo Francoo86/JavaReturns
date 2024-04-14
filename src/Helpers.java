391f7081101ba691998fb90a4dda4bb333a34bfa
Francoo86 <115720207+Francoo86@users.noreply.github.com>
2024-04-13 20:22:17 -0400

    add content parser for server

--- src/Helpers.java
+++ src/Helpers.java
@@ -1,5 +1,11 @@
 import Servidor.UDPServer;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Helpers {
     public static String clearWord(String word) {
         return word.trim();
@@ -20,4 +26,33 @@ public class Helpers {
         sb.append("]");
         return sb.toString();
     };
+
+    //this is for the server...
+    public static Collection<String> parseContents(String content){
+        // Define a regular expression pattern to match the integer and the list of strings
+        // Regular expression pattern to match the number and strings inside brackets
+        Pattern pattern = Pattern.compile("(\\d+):\\[(.*?)\\]");
+        Matcher matcher = pattern.matcher(content);
+
+        if(!matcher.find()) {
+            return null;
+        }
+
+        List<String> parsedContents = new ArrayList<>();
+
+        // Extracting the number
+        String number = matcher.group(1);
+        System.out.println("Numero del servicio: " + number);
+
+        parsedContents.add(number);
+
+        // Extracting the strings inside brackets
+        String[] strings = matcher.group(2).split(",\\s*");
+        for (String str : strings) {
+            parsedContents.add(str);
+            //System.out.println("String: " + str);
+        }
+
+        return parsedContents;
+    }
 }
